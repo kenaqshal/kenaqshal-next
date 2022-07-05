b4246447fb1b5faac0592eaae7a5c8261d299339
Ken Bramasta <ken.bramasta.mbiz.co.id>
2022-07-05 17:07:23 +0700

    feat: restore preview client sanity

---
+++ pages/api/preview.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { postBySlugQuery } from 'lib/queries';
+import { app } from '../../config/app';
+import { previewClient } from 'lib/sanity-server';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (
+    req.query.secret !== app.SANITY_PREVIEW_SECRET ||
+    !req.query.slug
+  ) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  const post = await previewClient.fetch(postBySlugQuery, {
+    slug: req.query.slug
+  });
+
+  if (!post) {
+    return res.status(401).json({ message: 'Invalid slug' });
+  }
+
+  res.setPreviewData({});
+  res.writeHead(307, { Location: `/blog/${post.slug}` });
+  res.end();
+}
