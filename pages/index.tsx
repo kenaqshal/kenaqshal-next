57ff4318e01edf8ff51fd9ad5a9c36c4c72f48e1
Ken Aqshal <kenaqshal31@gmail.com>
2022-07-09 21:27:20 +0700

    fix: svg property and link target

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,11 +47,11 @@ export default function Home({
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                   />
                 </svg>
