1da6c4dfc23d465f7997e1b678da195a3c0336e7
Ken Aqshal <kenaqshal31@gmail.com>
2022-07-09 21:18:46 +0700

    feat(mobile): redesign navbar

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,7 +10,6 @@ import Footer from 'components/Footer';
 import MobileMenu from 'components/MobileMenu';
 import Image from 'next/image';
 
-
 function NavItem({ href, text }) {
   const router = useRouter();
   const isActive = router.asPath === href;
@@ -96,40 +95,42 @@ export default function Container(props) {
             <NavItem href="/snippets" text="Snippets" />
             <NavItem href="/contact" text="Contact" />
           </div>
-          <button
-            aria-label="Toggle Dark Mode"
-            type="button"
-            className="w-10 h-10 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-4 ring-primary  transition-all"
-            onClick={() =>
-              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-            }
-          >
-            {mounted && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                className="w-5 h-5 text-gray-800 dark:text-gray-200"
-              >
-                {resolvedTheme === 'dark' ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                )}
-              </svg>
-            )}
-          </button>
+          <div className="hidden sm:flex">
+            <button
+              aria-label="Toggle Dark Mode"
+              type="button"
+              className="w-10 h-10 bg-gray-200 rounded-full dark:bg-gray-600 flex items-center justify-center  hover:ring-4 ring-primary  transition-all sm:text-right"
+              onClick={() =>
+                setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+              }
+            >
+              {mounted && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  className="w-5 h-5 text-gray-800 dark:text-gray-200"
+                >
+                  {resolvedTheme === 'dark' ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                  )}
+                </svg>
+              )}
+            </button>
+          </div>
         </nav>
       </div>
       <main
