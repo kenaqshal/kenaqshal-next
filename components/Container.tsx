977bad79d0f97f579866d2d0f572243374ba5acf
Ken Aqshal <kenaqshal31@gmail.com>
2022-07-09 16:45:15 +0700

    feat: change base color

--- components/Container.tsx
+++ components/Container.tsx
@@ -47,7 +47,7 @@ export default function Container(props) {
   };
 
   return (
-    <div className="bg-white-50 dark:bg-gray-900">
+    <div className="bg-gray-0 dark:bg-gray-900">
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
@@ -69,7 +69,7 @@ export default function Container(props) {
         )}
       </Head>
       <div className="flex flex-col justify-center px-8">
-        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-0  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
           <a href="#skip" className="skip-nav">
             Skip to content
           </a>
@@ -120,7 +120,7 @@ export default function Container(props) {
       </div>
       <main
         id="skip"
-        className="flex flex-col justify-center px-8 bg-white-50 dark:bg-gray-900"
+        className="flex flex-col justify-center px-8 bg-gray-0 dark:bg-gray-900"
       >
         {children}
         <Footer />
