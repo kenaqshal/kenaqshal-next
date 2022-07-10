4c7715c8d493d357fb62e98fac5c3eec32c3ec94
Ken Aqshal <kenaqshal31@gmail.com>
2022-07-10 21:31:33 +0700

    feat: sticky navbar

--- components/Container.tsx
+++ components/Container.tsx
@@ -70,11 +70,8 @@ export default function Container(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <div className="flex flex-col justify-center px-8">
-        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-0  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
-          <a href="#skip" className="skip-nav">
-            Skip to content
-          </a>
+      <div className="flex flex-col justify-center px-8 sticky top-0 z-50">
+        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto py-6  text-gray-900 bg-gray-0  dark:bg-gray-900 dark:text-gray-100">
           <a href="#skip" className="skip-nav">
             Skip to content
           </a>
