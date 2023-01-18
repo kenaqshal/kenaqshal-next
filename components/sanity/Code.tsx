b256797266595151690525bd5cf096ee0e51d836
Ken Aqshal Bramasta <ken.bramasta@mbiz.co.id>
2023-01-18 16:44:42 +0700

    fix: golang code doesnt show with highliter

--- components/sanity/Code.tsx
+++ components/sanity/Code.tsx
@@ -3,7 +3,9 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function SanityCode(props) {
-  const { language, code } = props.value;
+  let { language, code } = props.value;
+  if (language == 'golang') language = 'go';
+  
   return (
     <SyntaxHighlighter language={language} style={dracula} showLineNumbers>
       {code}
